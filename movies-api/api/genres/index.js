ca9a6358bfd5019cba29debb03e2f2fc28be8ce4
Sadge-Student <20092443@mail.wit.ie>
2022-11-26 14:11:47 +0000

    Mongo lab Exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,22 +1,12 @@
 import express from 'express';
 import { genres } from './genreData';
+import Genre from './genreModel';
 
 const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(genres);
-});
 
-router.get('/:id/genres', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    if (genres.id == id) {
-        res.status(200).json(genres);
-    } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
-    }
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
 
 export default router;
